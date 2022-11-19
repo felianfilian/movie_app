598ee70df3f78f11eca066133628dc7a2881697a
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-19 19:40:24 +0100

    fetch api data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const API_URL =
+  "https://www.themoviedb.org/discover/movie?sort_by=popularity.desc&api_key=e92c562473a1b2811265d470e02c6b93&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  "https://www.themoviedb.org/search/movie?api_key=e92c562473a1b2811265d470e02c6b93&query=''";
 
+// get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.results);
+}
