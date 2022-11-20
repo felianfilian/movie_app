8de8aaaddf9396414be0277daf42277cc4e1e5eb
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-20 10:36:30 +0100

    api test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://www.themoviedb.org/discover/movie?sort_by=popularity.desc&api_key=e92c562473a1b2811265d470e02c6b93&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc?api_key=e92c562473a1b2811265d470e02c6b93&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
   "https://www.themoviedb.org/search/movie?api_key=e92c562473a1b2811265d470e02c6b93&query=''";
